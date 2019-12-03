a68bd4a6ed841799b3e7bec314eef847100b190b
david.ampuero <david.ampuero@baobabsoluciones.es>
2019-12-03 12:10:18 +0100

    Update Readme and upload sln program

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # splitsqlfile
 Script on C# for split large sql file created by previous query
 # Compile
-This tool was created with VS2017 Community. Please open .sln file on main and build it.
+This tool was created with VS2017 Community for Windows OS. Please open .sln file on main and build it.
 # How it works
 1- Place a sql file named 'script.sql' on same folder of .exe
 
---
+++ split_sql/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace split_sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Splitting file 'script.sql' in 9500 lines separate files . . .");
+            try
+            {
+                if (File.Exists("script.sql"))
+                {
+                    string[] ss = File.ReadAllLines("script.sql");
+                    ss = ss.Skip(1).ToArray();
+
+                    int max_lines = 9500;
+                    int batch = 1;
+
+                    var chunk = ss.Take(max_lines);
+                    var rem = ss.Skip(max_lines);
+
+                    while (chunk.Take(1).Count() > 0)
+                    {
+                        String filename = "script_part_" + batch.ToString() + ".sql";
+                        using (StreamWriter sw = new StreamWriter(filename))
+                        {
+                            foreach (string line in chunk)
+                            {
+                                sw.WriteLine(line);
+                            }
+                        }
+                        chunk = rem.Take(max_lines);
+                        rem = rem.Skip(max_lines);
+                        batch++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not 'script.sql' found in this folder");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
